1235a29276074bea98fb54ba3d68880a3596213a
kazuya002ex <hara.ex002@gmail.com>
2019-09-13 13:08:32 +0900

    title edit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,23 @@
 module ApplicationHelper
   require "uri"
 
-def text_url_to_link text
+  def text_url_to_link text
+    URI.extract(text, ['http','https'] ).uniq.each do |url|
+      sub_text = ""
+      sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
 
-  URI.extract(text, ['http','https'] ).uniq.each do |url|
-    sub_text = ""
-    sub_text << "<a href=" << url << " target=\"_blank\">" << url << "</a>"
+      text.gsub!(url, sub_text)
+    end
 
-    text.gsub!(url, sub_text)
+    return text
   end
 
-  return text
-end
+  def full_title(page_title = '')
+    base_title = "OnelineNovelGame"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end

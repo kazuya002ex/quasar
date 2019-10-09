3bbdeab6e26e81637d384e6b4c060a63113f39c7
harakazuya <hara@ex-corp.co.jp>
2019-10-09 17:38:56 +0900

    fix1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: [:index]
   
   def index
     @users = User.all 

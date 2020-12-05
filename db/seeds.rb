cdbbb64caa892aec9a3118fb0f589a52a9b4bbd7
kazuya002ex <hara.ex002@gmail.com>
2020-12-05 19:51:02 +0900

    Create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+# User Create
+10.times do |n|
+  User.create!(
+    name: "hara#{n + 1}",
+    email: "aaaa#{n + 1}@gmail.comm",
+    password: "password",
+    password_confirmation: "password",
+    profile: "初めまして、hara#{n + 1}です"
+  )
+end

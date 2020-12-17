36c84b2eccdd4829247c9c90cefdb0a7b0f5be18
kazuya002ex <hara.ex002@gmail.com>
2020-12-17 10:58:47 +0900

    Fix dependent destroy config for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # 依頼人が消えても作品は残る
   has_many :posts
-  has_many :microposts, dependent: :destroy
+  # 著者が消えても投稿は残る
+  has_many :microposts
   has_many :messages, dependent: :destroy
   has_many :entries, dependent: :destroy
 end

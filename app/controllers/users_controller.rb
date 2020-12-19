4e05398aaaa8b7634b25db3a81eb06469eaab517
kazuya002ex <hara.ex002@gmail.com>
2020-12-19 13:28:24 +0900

    Fix user controller role

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,31 +1,31 @@
 class UsersController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:index]
+  skip_before_action :authenticate_user!, only: [:index, :show]
   
   def index
     @users = User.all 
   end
 
   def show
-    if user_signed_in?
-      @user=User.find(params[:id])
-      @currentUserEntry=Entry.where(user_id: current_user.id)
-      @userEntry=Entry.where(user_id: @user.id)
-      if @user.id == current_user.id
-      else
-        @currentUserEntry.each do |cu|
-          @userEntry.each do |u|
-            if cu.room_id == u.room_id then
-              @isRoom = true
-              @roomId = cu.room_id
-            end
-          end
-        end
-        if @isRoom
-        else
-          @room = Room.new
-          @entry = Entry.new
-        end
-      end
-    end
+    # if user_signed_in?
+      @user = User.find(params[:id])
+      # @currentUserEntry=Entry.where(user_id: current_user.id)
+      # @userEntry=Entry.where(user_id: @user.id)
+      # if @user.id == current_user.id
+      # else
+      #   @currentUserEntry.each do |cu|
+      #     @userEntry.each do |u|
+      #       if cu.room_id == u.room_id then
+      #         @isRoom = true
+      #         @roomId = cu.room_id
+      #       end
+      #     end
+      #   end
+      #   if @isRoom
+      #   else
+      #     @room = Room.new
+      #     @entry = Entry.new
+      #   end
+      # end
+    # end
   end
 end

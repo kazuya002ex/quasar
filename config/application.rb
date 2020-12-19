1d2420a23918c9a8b61628661a5d49ae5246e987
kazuya002ex <hara.ex002@gmail.com>
2020-12-19 21:35:00 +0900

    Fix config

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,8 @@ module NovelApp
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
 
+    config.assets.initialize_on_precompile = false
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading

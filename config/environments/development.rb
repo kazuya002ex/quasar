3c9b15c8085936580fc1e09ee64d00b43cb1473b
kazuya002ex <hara.ex002@gmail.com>
2020-12-19 20:55:08 +0900

    Fix config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,8 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.file_watcher = ActiveSupport::FileUpdateChecker
+
+
+  # production用
+  # config.hosts << ".herokuapp.com"
 end

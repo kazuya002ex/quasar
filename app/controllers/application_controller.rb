531afd71af81c653f0b47122122f1e3693150ed1
kazuya002ex <hara.ex002@gmail.com>
2020-12-24 08:43:13 +0900

    Add: UserIDを追加・表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :my_title, :img])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile, :my_title, :img])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :my_title, :img, :nickname])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile, :my_title, :img, :nickname])
     end
 end

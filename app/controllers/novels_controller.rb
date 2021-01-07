e51683a7207b7a14c88717562ce49af50f6f8620
kazuya002ex <chiharu.event2@gmail.com>
2021-01-07 13:06:27 +0900

    Refactor

--- app/controllers/novels_controller.rb
+++ app/controllers/novels_controller.rb
@@ -2,7 +2,7 @@ class NovelsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def show
-  	@post = Post.find(params[:id])
-  	@microposts = @post.microposts
+    @post = Post.find(params[:id])
+    @microposts = @post.microposts
   end
 end
--- app/models/block.rb
+++
@@ -1,8 +0,0 @@
-numbers = [1, 2, 3, 4]
-sum = 0
-numbers.each do |n|
-  sum_value = n.even? ? n * 10 : n
-  sum += sum_value
-end
-
-sum

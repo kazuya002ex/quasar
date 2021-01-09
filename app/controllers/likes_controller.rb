330d8ee44cf3863babbe623b2c4cff85fea005f7
kazuya002ex <chiharu.event2@gmail.com>
2021-01-09 21:26:24 +0900

    add: いいね完成！

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,21 +1,14 @@
 class LikesController < ApplicationController
-  before_action :set_post, only: [:create, :destroy]
 
   def create
     post = Post.find(params[:post_id])
-    like = Like.new(user_id: current_user.id, post_id: params[:post_id])
+    like = Like.new(user_id: current_user.id, post_id: post.id)
     redirect_back(fallback_location: root_path) if like.save
   end
 
   def destroy
     post = Post.find(params[:post_id])
-    like = Like.find_by(user_id: current_user.id, post_id: params[:post_id])
+    like = Like.find_by(user_id: current_user.id, post_id: post.id)
     redirect_back(fallback_location: root_path) if like.destroy
   end
-
-  private
-
-    def post_set
-      @post = Post.find(params[:id])
-    end
 end

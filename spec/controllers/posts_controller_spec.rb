283a96a2a936d0ffde666473b944bf3d999d8fa4
kazuya002ex <chiharu.event2@gmail.com>
2021-01-13 21:57:29 +0900

    feat: posts_controllerno

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
+  context '#index ユーザーが存在する場合' do
+    let(:users) { FactoryBot.create_list :user, 2 }
+
+    it 'リクエストが成功すること' do
+      get :index
+      expect(response.status).to eq 200
+    end
+
+    it 'indexテンプレートで表示されること' do
+      get :index
+      expect(response).to render_template :index
+    end
+  end
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,4 +1,15 @@
 FactoryBot.define do
+  factory :user do
+    name { "hoge12" }
+    email { |n| "hoge#{n}@example.com" }
+    password { "password" }
+    password_confirmation { "password" }
+
+    trait :invalid do
+      name nil
+    end
+  end
+
   factory :makoto, class: User do
     name { "Makoto" }
     email { "makoto@example.com" }

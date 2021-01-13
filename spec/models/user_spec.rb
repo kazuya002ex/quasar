6ee5d64ecea416337cbf874e91d9e3a5d0922354
kazuya002ex <chiharu.event2@gmail.com>
2021-01-13 11:16:10 +0900

    feat: UserモデルのSpec追加（emailカラムが絡むもの）

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,24 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context '正しい形式で入力した場合' do
-    user = User.new(
-        name: "name1",
-        email: "aaaa1@gmail.com",
-        password: "password",
-    )
-
-    it 'サインアップできる' do
-      expect(user).to be_valid
-    end
-  end
-
+  # ユーザー名のバリデーション
   context 'ユーザー名を空でサインアップした場合' do
     user = User.new(name: nil)
     user.valid?
 
     it '無効になる' do
-      expect(user.errors.full_messages).to include("ユーザー名を入力してください")
+      expect(user.errors.full_messages).to include('ユーザー名を入力してください')
       expect(user).to_not be_valid
     end
   end
@@ -28,7 +17,38 @@ RSpec.describe User, type: :model do
     user.valid?
 
     it '無効になる' do
-      expect(user.errors.full_messages).to include("ユーザー名は25文字以内で入力してください")
+      expect(user.errors.full_messages).to include('ユーザー名は25文字以内で入力してください')
+      expect(user).to_not be_valid
+    end
+  end
+
+  # メールアドレスのバリデーション
+  context 'メールアドレスを空でサインアップした場合' do
+    user = User.new(email: nil)
+    user.valid?
+
+    it '無効になる' do
+      expect(user.errors.full_messages).to include('メールアドレスを入力してください')
+      expect(user).to_not be_valid
+    end
+  end
+
+  context '既に存在するメールアドレスを入力した場合' do
+    user = User.create(
+      name: 'name1',
+      email: 'aaaa2@gmail.com',
+      password: 'password',
+    )
+
+    user = User.new(
+      name: 'name2',
+      email: 'aaaa2@gmail.com',
+      password: 'password',
+    )
+    user.valid?
+
+    it '無効になる' do
+      expect(user.errors.full_messages).to include('メールアドレスはすでに存在します')
       expect(user).to_not be_valid
     end
   end

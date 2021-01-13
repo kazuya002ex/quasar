675b51dabbf8d78de28606bd438a33f2e97b29ce
kazuya002ex <chiharu.event2@gmail.com>
2021-01-13 21:58:53 +0900

    refactor: posts_controllerのSpec

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -2,8 +2,6 @@ require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
   context '#index ユーザーが存在する場合' do
-    let(:users) { FactoryBot.create_list :user, 2 }
-
     it 'リクエストが成功すること' do
       get :index
       expect(response.status).to eq 200

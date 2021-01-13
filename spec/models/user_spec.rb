734909c119e867532cc3caed72f0d5f15ca712a8
kazuya002ex <chiharu.event2@gmail.com>
2021-01-13 11:06:07 +0900

    feat: userモデルSpec作成（nameカラムに絡むもの）

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,4 +12,24 @@ RSpec.describe User, type: :model do
       expect(user).to be_valid
     end
   end
+
+  context 'ユーザー名を空でサインアップした場合' do
+    user = User.new(name: nil)
+    user.valid?
+
+    it '無効になる' do
+      expect(user.errors.full_messages).to include("ユーザー名を入力してください")
+      expect(user).to_not be_valid
+    end
+  end
+
+  context 'ユーザー名が25文字を超える場合' do
+    user = User.new(name: 'a' * 26)
+    user.valid?
+
+    it '無効になる' do
+      expect(user.errors.full_messages).to include("ユーザー名は25文字以内で入力してください")
+      expect(user).to_not be_valid
+    end
+  end
 end

3c2da51379c711079f84ad634e8e508151b7d8f4
kazuya002ex <chiharu.event2@gmail.com>
2021-01-14 11:59:40 +0900

    fix: 警告に則った変更

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,6 +26,5 @@ RSpec.configure do |config|
 
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
-  config.include Devise::TestHelpers, type: :controller
   config.include ControllerMacros, type: :controller
 end

efe51fb41a2f32487a0308adaefb22db5d5605ef
kazuya002ex <chiharu.event2@gmail.com>
2021-01-19 17:27:27 +0900

    feat: Specを修正

--- spec/controllers/microposts_controller_spec.rb
+++ spec/controllers/microposts_controller_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 RSpec.describe MicropostsController, type: :controller do
   let(:makoto) { FactoryBot.create :makoto }
+  let!(:post) { FactoryBot.create :post }
+  # let!(:micropost1_1) { create(:micropost1_1, post: post) }
   before { login_user makoto }
 
   context '#create ユーザーが存在する場合' do
     it 'リクエストが成功すること' do
-      post :create, params: { micropost: FactoryBot.attributes_for(:micropost) }
+      post :create, params: { micropost: FactoryBot.attributes_for(:micropost, post_id: post.id, user_id: makoto.id) }
       expect(response.status).to eq 302
     end
 
--- spec/factories/micropost.rb
+++ spec/factories/micropost.rb
@@ -1,9 +1,20 @@
 FactoryBot.define do
   factory :micropost do
     content { "たのしい文章です" }
+    user_id { 1 }
+    post_id { 1 }
+    association :post
 
     trait :invalid do
       content { nil }
+      user_id { 1 }
+      post_id { 1 }
+    end
+  end
+
+  %w(micropost1_1 micropost1_2 micropost2_1).each do |micropost|
+    factory micropost.to_sym, class: Micropost do
+      content { '文章文章is文章' }
     end
   end
 end

5e52825193bbe48b493013f54f35e4e37240587f
kazuya002ex <chiharu.event2@gmail.com>
2021-01-21 16:51:19 +0900

    feat: static controllerを作成 & lpをstaticni

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,15 @@ Rails.application.routes.draw do
     resources :microposts, only: [:create, :destroy]
     post :confirm, action: :confirm_new, on: :new
 
-    post "add", to: "likes#create"
-    delete "/add", to: "likes#destroy"
+    post 'add', to: 'likes#create'
+    delete '/add', to: 'likes#destroy'
   end
 
   resources :users, only: [:index, :show]
   resources :novels, only: [:show]
   resources :messages, only: [:create]
   resources :rooms, only: [:create, :show, :index]
+
+  # 静的ページ
+  get '/lp', to: 'static#lp'
 end

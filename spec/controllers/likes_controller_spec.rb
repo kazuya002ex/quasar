4d19b37a4fc6e12a937965a97f32c1d5c8da05a3
kazuya002ex <chiharu.event2@gmail.com>
2021-01-21 11:47:54 +0900

    feat: likes_controllerのSpecを作成（RED）

--- spec/controllers/likes_controller_spec.rb
+++ spec/controllers/likes_controller_spec.rb
@@ -1,4 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe LikesController, type: :controller do
+  let(:makoto) { FactoryBot.create :makoto }
+  before { login_user makoto }
+  let(:yuuki) { FactoryBot.create :yuuki }
+  before { login_user yuuki }
+
+  # POSTメソッド
+  context '#create ユーザーが存在する場合' do
+    it 'リクエストが成功すること' do
+      post :create, params: { like: FactoryBot.attributes_for(:like) }
+      expect(response.status).to eq 302
+    end
+
+    it 'いいねが作成されること' do
+      expect do
+        post :create, params: { like: FactoryBot.attributes_for(:like) }
+      end.to change(Post, :count).by(1)
+    end
+
+    it 'リダイレクトすること' do
+      post :create, params: { like: FactoryBot.attributes_for(:like) }
+      expect(response).to redirect_to Like.last
+    end
+  end
 end

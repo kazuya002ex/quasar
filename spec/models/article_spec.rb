c8149fab07ea998fd7dcc7141cc43115b8935e7a
kazuya002ex <chiharu.event2@gmail.com>
2021-01-26 18:22:19 +0900

    feat: ArticleのTitleに関わるSpecの作成

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'タイトルが空の場合' do
+    article = Article.new(title: nil)
+    article.valid?
+
+    it '無効になる' do
+      expect(article.errors.full_messages).to include('Titleを入力してください')
+      expect(article).to_not be_valid
+    end
+  end
+
+  context 'タイトルが32文字を超える場合' do
+    article = Article.new(title: '今回の変更はあなたの人生を変える、、、、いや、、、、そんな生優しいものじゃあらへんで、、、わかったか？')
+    article.valid?
+    
+    it '無効になる' do
+      expect(article.errors.full_messages).to include('Titleは32文字以内で入力してください')
+      expect(article).to_not be_valid
+    end
+  end
 end

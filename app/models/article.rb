6f8d1775e7aded51de671c864ba0bb918b1e5f39
kazuya002ex <chiharu.event2@gmail.com>
2021-01-26 18:15:23 +0900

    feat: articleにModelバリデーション追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+  belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 32 }
+  validates :content, presence: true, length: { maximum: 255 }
 end

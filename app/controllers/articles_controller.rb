f55197e5b349436f9a8c56ef777e5c12ee9ca62d
kazuya002ex <chiharu.event2@gmail.com>
2021-01-27 11:01:35 +0900

    fix: 軽微

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -54,7 +54,7 @@ class ArticlesController < ApplicationController
   private
 
     def article_params
-      params.require(:artcile).permit(:title, :content)
+      params.require(:article).permit(:title, :content)
     end
 
     def set_article

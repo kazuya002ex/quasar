778f960fd30b1a46f04c6a701c6db0b920803ed1
kazuya002ex <chiharu.event2@gmail.com>
2021-02-16 11:39:58 +0900

    feat: GenreのSpecを1つ目作成

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Genre, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '名前が空の場合' do
+    genre = Genre.new(name: nil)
+    genre.valid?
+
+    it '無効になる' do
+      expect(genre.errors.full_messages).to include('Nameを入力してください')
+      expect(genre).to_not be_valid
+    end
+  end
 end

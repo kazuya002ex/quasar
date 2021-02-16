2962bd236728ae4d03e36d2e712f434eee6e44ab
kazuya002ex <chiharu.event2@gmail.com>
2021-02-16 11:43:14 +0900

    feat: ジャンル名の文字数制限

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -10,4 +10,14 @@ RSpec.describe Genre, type: :model do
       expect(genre).to_not be_valid
     end
   end
+
+  context 'ジャンル名が20文字を超える場合' do
+    genre = Genre.new(name: '漆黒に誘われし闇の創生神が降臨し極光の稲妻が啓明するならばお芋')
+    genre.valid?
+    
+    it '無効になる' do
+      expect(genre.errors.full_messages).to include('Nameは20文字以内で入力してください')
+      expect(genre).to_not be_valid
+    end
+  end
 end

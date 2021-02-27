436107d04e9507a1a1bb55ffdd8d5f64593a84f9
kazuya002ex <chiharu.event2@gmail.com>
2021-02-27 16:00:24 +0900

    feat: クラスを動的に変更する

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,10 @@ class HomeController < ApplicationController
   def top
     @q = Post.ransack(params[:q])
     @posts = @q.result(distinct: true).order(id: "DESC")
+
+    # top-article
+    @sample_article = Article.first
+
     # top-raiking
     @ranking_posts = Post.rank
     #top-new-arrival

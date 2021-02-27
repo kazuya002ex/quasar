5cc2377f3db7dee7ed035e95ca03e5d3685257e5
kazuya002ex <chiharu.event2@gmail.com>
2021-02-27 15:22:08 +0900

    feat: articleのtypeにvalidateを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,5 @@ class Article < ApplicationRecord
 
   validates :title, presence: true, length: { maximum: 30 }
   validates :content, presence: true, length: { maximum: 255 }
+  validates :type, presence: true, length: { maximum: 3 }
 end

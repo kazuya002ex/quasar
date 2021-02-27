4472cb99afda45c2194d1802e03ab566e9e9d11d
kazuya002ex <chiharu.event2@gmail.com>
2021-02-27 15:12:50 +0900

    feat: それに伴いSpecも修正

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -12,12 +12,12 @@ RSpec.describe Article, type: :model do
     end
   end
 
-  context 'タイトルが32文字を超える場合' do
+  context 'タイトルが30文字を超える場合' do
     article = Article.new(title: '今回の変更はあなたの人生を変える、、、、いや、、、、そんな生優しいものじゃあらへんで、、、わかったか？')
     article.valid?
     
     it '無効になる' do
-      expect(article.errors.full_messages).to include('Titleは32文字以内で入力してください')
+      expect(article.errors.full_messages).to include('Titleは30文字以内で入力してください')
       expect(article).to_not be_valid
     end
   end

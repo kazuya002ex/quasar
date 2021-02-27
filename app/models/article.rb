a18fea36d7e6aa8bf4b3ba3d5bf65540533ce3d1
kazuya002ex <chiharu.event2@gmail.com>
2021-02-27 15:37:53 +0900

    feat: 正しいかわからないenumを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Article < ApplicationRecord
+  enum :news_type, { other: 0, notice: 1, update: 2 }, _prefix: true
+
   belongs_to :user
 
   validates :title, presence: true, length: { maximum: 30 }

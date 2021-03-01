5a4e9f642dd2d75307beebc6b2551c263854d673
kazuya002ex <chiharu.event2@gmail.com>
2021-03-01 16:18:44 +0900

    feat: indexページにも管理者制限をかけておく

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@
 
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
-  before_action :authenticatte_admin!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticatte_admin!, only: [:index, :new, :create, :edit, :update, :destroy]
   skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index

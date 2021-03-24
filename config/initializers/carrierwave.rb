1fcd8fa8df2d2fe3cad1a14f6795779454bde887
kazuya002ex <chiharu.event2@gmail.com>
2021-03-24 17:50:12 +0900

    feat: やばやばだよ〜

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: 'AKIAXMS6RKK74TRBACGD',
-    aws_secret_access_key: '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',
+    aws_access_key_id: '',
+    aws_secret_access_key: '',
     region: 'ap-northeast-1',
     }
 

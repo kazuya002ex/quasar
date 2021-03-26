852ecbdfaefd8ceee49f3f4294df96eb58c01f85
kazuya002ex <chiharu.event2@gmail.com>
2021-03-26 18:13:01 +0900

    feat: s3設定による変更

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,25 @@
+CarrierWave.configure do |config|
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['S3_ACCESS_KEY'],
+    aws_secret_access_key: ENV['S3_SECRET_KEY'],
+    region: ENV['S3_REGION'],
+    }
+
+  # 公開・非公開の切り替え
+  config.fog_public     = true
+  # キャッシュの保存期間
+  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
+
+  # キャッシュをS3に保存
+  # config.cache_storage = :fog
+
+  config.fog_directory = 'relier-s3-production-image-store'
+  config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/relier-s3-production'
+  config.asset_host = 'https://relier-s3-production-image-store.s3-ap-northeast-1.amazonaws.com'
+  config.cache_storage = :fog
+end
+  
+# 日本語ファイル名の設定
+CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/

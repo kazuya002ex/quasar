ee9e68127e0aa3bb8dd0eab757676f12fd94998b
kazuya002ex <chiharu.event2@gmail.com>
2021-04-15 14:30:33 +0900

    feat: micropostsコントローラーの書き込み制限

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -4,6 +4,12 @@ class MicropostsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
+
+    if @post.closed
+      flash[:alert] = 'この作品は新規の書き込みを停止しています'
+      redirect_back(fallback_location: root_path)
+    end
+
     @micropost = Micropost.new(micropost_params)
     @micropost.user_id = current_user.id
     @micropost.post_id = @post.id

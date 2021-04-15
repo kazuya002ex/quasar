f2e728dbd3bf2fe250aabf7b6192b2685e666a5a
kazuya002ex <chiharu.event2@gmail.com>
2021-04-15 14:22:37 +0900

    feat: closedPostsコントローラーを作成

---
+++ app/controllers/closed_posts_controller.rb
@@ -0,0 +1,13 @@
+class ClosedPostsController < ApplicationController
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(closed: true)
+      success(text: '作品への書き込みを停止しました')
+      redirect_to @post
+    else
+      flash[:alert] = '作品への書き込み停止に失敗しました（停止の処理は完了していません）'
+      render :edit
+    end
+  end
+end

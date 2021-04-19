11e0844b3f259cee7ef0318e7937b19eec2f69ee
kazuya002ex <chiharu.event2@gmail.com>
2021-04-19 12:42:51 +0900

    feat: genre createをrake taskに移動

--- db/seeds.rb
+++ db/seeds.rb
@@ -57,13 +57,3 @@ Article.create!(title: "暇だね〜",
                 user_id: 1,
                 news_type: 0
                 )
-
-p "Genre create"
-Genre.create!(name: '推理')
-Genre.create!(name: '青春')
-Genre.create!(name: '恋愛')
-Genre.create!(name: 'SF')
-Genre.create!(name: 'ファンタジー')
-Genre.create!(name: 'ホラー')
-Genre.create!(name: 'ノンフィクション')
-Genre.create!(name: 'スポーツ')
